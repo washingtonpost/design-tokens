00069a213e6b57d7858380d3fbee0cb09cde7007
Maria Eguiluz <mbeguiluz@gmail.com>
2022-06-28 15:23:53 -0400

    fix: remove font-files and references

--- fonts/subsetFonts.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const typographyTokens = require("../packages/typography/tokens.json");
-const glyphs = typographyTokens.typography.glyphs.value;
-
-// At the moment, glyphhanger works best reading glyphs from a file
-fs.writeFileSync(__dirname + "/supported-characters.txt", glyphs)
-
-// TODO: Subset fonts entirely from this script. This will require some updates from glyphhanger
